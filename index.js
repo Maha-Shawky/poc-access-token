b9c000dc27763d8ef9e1d80e383efa88b4e5c3af
Maha Saad <maha.saad.mohamed@gmail.com>
2019-10-18 16:45:28 +0200

    initial

---
+++ index.js
@@ -0,0 +1,68 @@
+const callsWriter = require('./src/Utils/callsWriter')
+const FaceBookAPI = require('./src/FaceBookAPI');
+const { TokenManager, CallType } = require('./src/TokenManager');
+
+/**
+ * {
+  "error": {
+    "message": "(#4) Application request limit reached",
+    "type": "OAuthException",
+    "is_transient": true,
+    "code": 4,
+    "fbtrace_id": "Ab62z_9wKQFgamX69q96Wno"
+  }
+}
+* - Bad request => expire
+* - Forbidden => rate limit call count, total count,
+*/
+
+const tokenManager = new TokenManager();
+const fbApi = new FaceBookAPI({ tokenManager })
+
+
+
+
+
+continuosCallFBookAndLog = async(url, fileName, writeInfile) => {
+
+    while (true) {
+        try {
+            const res = await fbApi.callFb(url);
+
+            if (writeInfile)
+                await callsWriter.appendToJsonFile(fileName, res);
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+
+(async() => {
+
+    tokenManager.addNewToken('EAAYgrrV0uxYBABsX93y8JXakqdYtHfEDNVnp04JcaJIvAOKXmCP1gSVZCad57H8jlfknJCu7MIZAoZCpgsqsyZCQvZBP9uR2bvJnKsko7lLvQchBfbLWQfHHJzXDkEF4tbpZCLCCTnuHa5i1JfPNEv0dfuezyCIEvB0Cqg4Ce8zGDW0wXmeyNFXAehwczxqxYZD');
+    tokenManager.addNewToken('EAALDOhsLbBIBACZCD3tjCjTb3tEVFNthGA5u7jsn6n1j1lvHd7jUc5NJVhnHtMuukIn2jA8byK4YzdtI9JKfac0zRHnmPmg2kn35zSUZAj4LA9R92wUHLgb3h49G3Gdj6tSlp014aeZAMlMSRnvpbGYZBPOBY2rGwDehSrl8ZA9w235QUB4JU49t8ebpaaJMIZAC34fiaXaQZDZD');
+
+    const check = 1;
+    switch (check) {
+        case 0:
+            {
+                const totalDuration = await callsWriter.setDurationForEachCall('results.json');
+                console.log(`totalDuration: ${totalDuration} seconds`); //256
+                break;
+            }
+        case 1:
+            {
+                const url = `https://graph.facebook.com/v4.0/me/feed?limit=100&`;
+                await continuosCallFBookAndLog(url, 'results.json', false)
+                break;
+            }
+        case 2:
+            {
+                const url = `https://graph.facebook.com/v4.0/me?fields=id,name&`;
+                await fbApi.callFb(url)
+                break;
+            }
+    }
+})()
---
+++ src/Utils/callsWriter.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+
+module.exports = {
+    appendToJsonFile: (fileName, jsonObj) => {
+
+        return new Promise((resolve, reject) => {
+
+            fs.readFile(fileName, function(err, data) {
+                if (err)
+                    reject(err);
+
+                var json = JSON.parse(data)
+                json.push(jsonObj)
+
+                fs.writeFile(fileName, JSON.stringify(json, null, 4), (err, data) => {
+                    if (err)
+                        return reject(err);
+
+                    resolve(data);
+                })
+            })
+        })
+    },
+
+    setDurationForEachCall: async(fileName) => {
+        return new Promise((resolve, reject) => {
+            fs.readFile(fileName, function(err, data) {
+                if (err)
+                    reject(err);
+
+                var jsonArray = JSON.parse(data)
+                let totalDuration = 0;
+
+                jsonArray = jsonArray.map((obj, i) => {
+                    if (i === 0)
+                        return obj;
+
+                    const diffTime = Math.abs(new Date(obj.date) - new Date(jsonArray[i - 1].date));
+                    const diffSec = Math.round(diffTime / (1000));
+
+                    if (obj.code === 'OK') totalDuration += diffSec;
+
+                    return {...obj, durationInSec: diffSec };
+                })
+                fs.writeFile(fileName, JSON.stringify(jsonArray, null, 4), (err, data) => {
+                    if (err)
+                        reject(err);
+
+                    resolve(totalDuration);
+                })
+            })
+        })
+    },
+
+    testLogToFile: async(fileName) => {
+        const data = {
+            call_count: 10,
+            total_cputime: 20,
+            total_time: 30,
+            code: 200,
+            date: new Date()
+        }
+
+        for (i = 0; i < 10; i++) {
+            await this.appendToJsonFile(fileName, data);
+        }
+    }
+}
