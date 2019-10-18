185c793abf69d8e09527cc2a805d8f58cbb24e9d
Maha Saad <maha.saad.mohamed@gmail.com>
2019-10-18 19:04:05 +0200

    add setup

--- index.js
+++ index.js
@@ -34,11 +34,22 @@ continuosCallFBookAndLog = async(url, fileName, writeInfile) => {
     }
 }
 
+setUp = async() => {
+    const accessTokens = [
+        'EAAYgrrV0uxYBABsX93y8JXakqdYtHfEDNVnp04JcaJIvAOKXmCP1gSVZCad57H8jlfknJCu7MIZAoZCpgsqsyZCQvZBP9uR2bvJnKsko7lLvQchBfbLWQfHHJzXDkEF4tbpZCLCCTnuHa5i1JfPNEv0dfuezyCIEvB0Cqg4Ce8zGDW0wXmeyNFXAehwczxqxYZD',
+        'EAALDOhsLbBIBACZCD3tjCjTb3tEVFNthGA5u7jsn6n1j1lvHd7jUc5NJVhnHtMuukIn2jA8byK4YzdtI9JKfac0zRHnmPmg2kn35zSUZAj4LA9R92wUHLgb3h49G3Gdj6tSlp014aeZAMlMSRnvpbGYZBPOBY2rGwDehSrl8ZA9w235QUB4JU49t8ebpaaJMIZAC34fiaXaQZDZD'
+    ]
+
+    for (i = 0; i < accessTokens.length; i++) {
+        const accessToken = accessTokens[i];
+        await fbApi.callByAccessToken('https://graph.facebook.com/v4.0/me?fields=id,name&', accessToken)
+    }
+
+}
 
 (async() => {
 
-    tokenManager.addNewToken('EAAYgrrV0uxYBABsX93y8JXakqdYtHfEDNVnp04JcaJIvAOKXmCP1gSVZCad57H8jlfknJCu7MIZAoZCpgsqsyZCQvZBP9uR2bvJnKsko7lLvQchBfbLWQfHHJzXDkEF4tbpZCLCCTnuHa5i1JfPNEv0dfuezyCIEvB0Cqg4Ce8zGDW0wXmeyNFXAehwczxqxYZD');
-    tokenManager.addNewToken('EAALDOhsLbBIBACZCD3tjCjTb3tEVFNthGA5u7jsn6n1j1lvHd7jUc5NJVhnHtMuukIn2jA8byK4YzdtI9JKfac0zRHnmPmg2kn35zSUZAj4LA9R92wUHLgb3h49G3Gdj6tSlp014aeZAMlMSRnvpbGYZBPOBY2rGwDehSrl8ZA9w235QUB4JU49t8ebpaaJMIZAC34fiaXaQZDZD');
+    await setUp();
 
     const check = 2;
     switch (check) {
@@ -56,7 +67,7 @@ continuosCallFBookAndLog = async(url, fileName, writeInfile) => {
             }
         case 2:
             {
-                const url = `https://graph.facebook.com/v4.0/me?fields=id,name&`;
+                const url = 'https://graph.facebook.com/v4.0/me?fields=id,name&';
                 const tokenInfo = await fbApi.call(url)
                 console.log(tokenInfo);
                 break;
